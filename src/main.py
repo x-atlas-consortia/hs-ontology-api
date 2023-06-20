41bacda7f91e197a51314811e8bd15df95ea1a21
SamSedivy <sls325@pitt.edu>
2023-06-20 14:22:11 -0400

    Add datasets endpoint

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from flask import Flask, jsonify
 from pathlib import Path
 from ubkg_api.app import UbkgAPI, logger
 
+from src.routes.datasets.datasets_controller import datasets_blueprint
 from src.routes.organs.organs_controller import organs_blueprint
 from src.routes.valueset.valueset_controller import valueset_blueprint
 
@@ -12,6 +13,7 @@ flask_app.config.from_pyfile('app.cfg')
 app = UbkgAPI(flask_app.config).app
 app.register_blueprint(valueset_blueprint)
 app.register_blueprint(organs_blueprint)
+app.register_blueprint(datasets_blueprint)
 
 
 # Define the /status endpoint in the ubkg_api package will causes 500 error
