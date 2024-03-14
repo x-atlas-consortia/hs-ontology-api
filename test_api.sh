53b7bab18441067808a95bf630b159af82e38c14
AlanSimmons <jas971@pitt.edu>
2024-03-14 11:52:43 -0500

    renamed application parameter in field-entities to application_context

--- test_api.sh
+++ test_api.sh
@@ -166,17 +166,17 @@ echo
 # Test for celltypes list.
 echo "celltypes-info GET"
 curl --request GET \
- --url "${UBKG_URL}/celltypes-info?page=1&proteins_per_page=3" \
+ --url "${UBKG_URL}/celltypes-info?page=1&celltypes_per_page=3" \
  --header "Content-Type: application/json" |cut -c1-60
 echo
 echo "celltypes-info GET last page"
 curl --request GET \
- --url "${UBKG_URL}/celltypes-info?page=last&proteins_per_page=3" \
+ --url "${UBKG_URL}/celltypes-info?page=last&celltypes_per_page=3" \
  --header "Content-Type: application/json" |cut -c1-60
 echo
 echo "celltypes-info GET starts_with B"
 curl --request GET \
- --url "${UBKG_URL}/celltypes-info?proteins_per_page=3&starts_with=B" \
+ --url "${UBKG_URL}/celltypes-info?celltypes_per_page=3&starts_with=B" \
  --header "Content-Type: application/json" |cut -c1-60
 echo
 # Test for proteins endpoint.
